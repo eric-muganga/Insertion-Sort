a120824e3661bbc217b294b1241af53f21abc590
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2023-05-18 01:27:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Time complexity is O(n^2) and space complexity is O(1)
+
+int[] arr = { -4, 5, 10, 8, -10, -6, -4, -2, -5, 3, 5, -4, -5, -1, 1, 6, -7, -6, -7, 8 };
+
+int[] sortedArray = InsertionSort(arr);
+
+Console.WriteLine("{" + string.Join(", ", sortedArray) + "}");
+
+int[] InsertionSort(int[] array)
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        int key = array[i];
+        int j = i - 1;
+
+        while (j >= 0 && array[j] > key)
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = key;
+    }
+    return array;
+}
